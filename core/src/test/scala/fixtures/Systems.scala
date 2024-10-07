020825579b1c89595c6bf258bba7749784405d78
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2024-10-08 00:56:56 +0200

    Add fixtures to core module tests

--- core/src/test/scala/fixtures/Systems.scala
+++ core/src/test/scala/fixtures/Systems.scala
@@ -1,19 +1,20 @@
-package sprint_prototype
+package fixtures
 
-import core.System
-import core.World
+import core.{System, World}
 
 // Movement system: updates position using speed
 class MovementSystem extends System:
+
   override def update(world: World): Unit =
     for entity <- world.getEntities do
       (world.getComponent[Position](entity), world.getComponent[Speed](entity)) match
-        case (Some(pos), Some(speed)) =>
-          world.addComponent(entity, pos.copy(pos.x + speed.vx, pos.y + speed.vy))
-        case _ =>
+      case (Some(pos), Some(speed)) =>
+        world.addComponent(entity, pos.copy(pos.x + speed.vx, pos.y + speed.vy))
+      case _ =>
 
 // Collision system: if entities collide, set speed to zero
 class CollisionSystem extends System:
+
   override def update(world: World): Unit =
     val entities = world.getEntities
     for i <- entities.indices do
@@ -21,8 +22,8 @@ class CollisionSystem extends System:
         val entityA = entities(i)
         val entityB = entities(j)
         (world.getComponent[Position](entityA), world.getComponent[Position](entityB)) match
-          case (Some(posA), Some(posB)) if posA.x == posB.x && posA.y == posB.y =>
-            println(s"Collision detected between Entity ${entityA.id} and Entity ${entityB.id}")
-            world.addComponent(entityA, Speed(0,0))
-            world.addComponent(entityB, Speed(0,0))
-          case _ =>
+        case (Some(posA), Some(posB)) if posA.x == posB.x && posA.y == posB.y =>
+          println(s"Collision detected between Entity ${entityA.id} and Entity ${entityB.id}")
+          world.addComponent(entityA, Speed(0, 0))
+          world.addComponent(entityB, Speed(0, 0))
+        case _ =>
