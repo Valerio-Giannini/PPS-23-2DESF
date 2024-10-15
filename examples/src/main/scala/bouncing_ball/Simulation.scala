6fe0d64a922556a18662710a72add64545cc4759
rares vasiliu <rv.vasiliu@gmail.com>
2024-10-15 13:17:08 +0200

    Another minor change

--- examples/src/main/scala/bouncing_ball/Simulation.scala
+++ examples/src/main/scala/bouncing_ball/Simulation.scala
@@ -21,13 +21,6 @@ object Simulation:
     for tick <- 1 to 10 do
       println(s"Tick $tick")
       world.update()
-      for entity <- world.getEntities do
-        val position =
-          world.getComponent[Position](entity).map(pos => s"Position(${pos.x}, ${pos.y})").getOrElse("No Position")
-        val speed =
-          world.getComponent[Speed](entity).map(speed => s"Speed(${speed.vx}, ${speed.vy})").getOrElse("No Speed")
-        println(s"Entity ${entity.id}: $position, $speed")
-      println("-------------------")
 
 @main def runSimulation(): Unit =
   start()
