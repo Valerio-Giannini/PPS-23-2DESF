d37176e4d390c0a92c6055d1ab0444e104308c4d
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2024-10-24 17:07:34 +0200

    add entity removal to World

--- core/src/main/scala/updated_core/World.scala
+++ core/src/main/scala/updated_core/World.scala
@@ -4,6 +4,8 @@ trait World:
   def entities: Iterable[Entity]
   def createEntity(components: Component*): Entity
   def addEntity(entity: Entity): World
+  def removeEntity(entity: Entity): World
+  def clearEntities(): World
 
 class SimpleWorld extends World:
   private var archetypes: Map[Set[ComponentTag[_]], Archetype] = Map.empty
@@ -12,21 +14,32 @@ class SimpleWorld extends World:
     archetypes.values.flatMap(_.entities)
 
   def createEntity(components: Component*): Entity =
-    val newEntity = Entity(components: _*)
+    val newEntity = Entity(components*)
     addEntity(newEntity)
     newEntity
-  
+
   def addEntity(entity: Entity): World =
     getArchetype(entity) match
-      case Some(archetype) =>
-        archetype.add(entity)
-      case None =>
-        val componentTags = entity.componentTags
-        val newArchetype = Archetype(componentTags)
-        newArchetype.add(entity)
-        archetypes += (newArchetype.componentTags -> newArchetype)
+    case Some(archetype) =>
+      archetype.add(entity)
+    case None =>
+      val componentTags = entity.componentTags
+      val newArchetype  = Archetype(componentTags)
+      newArchetype.add(entity)
+      archetypes += (newArchetype.componentTags -> newArchetype)
     this
 
   private def getArchetype(entity: Entity): Option[Archetype] =
     val componentTags = entity.componentTags
     archetypes.get(componentTags)
+
+  def removeEntity(entity: Entity): World =
+    getArchetype(entity) match
+    case Some(archetype) =>
+      archetype.remove(entity)
+    case None =>
+    this
+
+  def clearEntities(): World =
+    archetypes.values.foreach(_.clearEntities())
+    this
--- core/src/test/scala/updated_core/WorldTest.scala
+++ core/src/test/scala/updated_core/WorldTest.scala
@@ -31,3 +31,24 @@ class WorldTest extends AnyWordSpec with Matchers with BeforeAndAfterEach:
         val entity = world.createEntity(C1(1), C1(2))
         world.entities should contain(entity)
         entity.componentTags should have size 1
+      "allow the removal of an entity" in :
+        val entityA = world.createEntity()
+        val entityB = world.createEntity()
+        world.removeEntity(entityB)
+        world.entities should have size 1
+      "allow the removal and reinsertion of an entity with its components" in :
+        val entity = world.createEntity(C1(1), C2(2))
+        world.entities should contain(entity)
+        world.removeEntity(entity)
+        world.entities shouldBe empty
+        world.addEntity(entity)
+        world.entities should contain(entity)
+      "do nothing when trying to remove a non-existent entity" in :
+        val outWorldEntity = Entity()
+        world.removeEntity(outWorldEntity)
+        world.entities shouldBe empty
+      "allow to clear all of them" in:
+        val numEntities = 10
+        List.fill(numEntities)(world.createEntity())
+        world.clearEntities()
+        world.entities shouldBe empty
