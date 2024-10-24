0ebf38f6ccd44436a45f1129ecdbf897adeb0bec
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2024-10-24 17:01:47 +0200

    add entity creation and addition to World

--- core/src/main/scala/updated_core/World.scala
+++ core/src/main/scala/updated_core/World.scala
@@ -2,9 +2,31 @@ package updated_core
 
 trait World:
   def entities: Iterable[Entity]
-  
+  def createEntity(components: Component*): Entity
+  def addEntity(entity: Entity): World
+
 class SimpleWorld extends World:
   private var archetypes: Map[Set[ComponentTag[_]], Archetype] = Map.empty
-  
+
   def entities: Iterable[Entity] =
     archetypes.values.flatMap(_.entities)
+
+  def createEntity(components: Component*): Entity =
+    val newEntity = Entity(components: _*)
+    addEntity(newEntity)
+    newEntity
+  
+  def addEntity(entity: Entity): World =
+    getArchetype(entity) match
+      case Some(archetype) =>
+        archetype.add(entity)
+      case None =>
+        val componentTags = entity.componentTags
+        val newArchetype = Archetype(componentTags)
+        newArchetype.add(entity)
+        archetypes += (newArchetype.componentTags -> newArchetype)
+    this
+
+  private def getArchetype(entity: Entity): Option[Archetype] =
+    val componentTags = entity.componentTags
+    archetypes.get(componentTags)
--- core/src/test/scala/updated_core/WorldTest.scala
+++ core/src/test/scala/updated_core/WorldTest.scala
@@ -15,3 +15,19 @@ class WorldTest extends AnyWordSpec with Matchers with BeforeAndAfterEach:
     "initialized" should:
       "be empty" in:
         world.entities shouldBe empty
+    "managing entity" should:
+      "allow adding an already existing entity" in :
+        val entity = Entity()
+        world.addEntity(entity)
+        world.entities should contain (entity)
+      "allow creation of an entity without components" in :
+        val entity = world.createEntity()
+        world.entities should contain(entity)
+      "allow creation of an entity with multiple components" in :
+        val entity = world.createEntity(C1(1), C2(2))
+        world.entities should contain(entity)
+        entity.componentTags should have size 2
+      "allow creation of an entity without duplicate components" in :
+        val entity = world.createEntity(C1(1), C1(2))
+        world.entities should contain(entity)
+        entity.componentTags should have size 1
