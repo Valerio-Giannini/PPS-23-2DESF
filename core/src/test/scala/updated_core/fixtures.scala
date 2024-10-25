cc93047931c201299b974bb9a54d13b7397996e0
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2024-10-25 15:52:21 +0200

    updating fixtures system

--- core/src/test/scala/updated_core/fixtures.scala
+++ core/src/test/scala/updated_core/fixtures.scala
@@ -6,7 +6,11 @@ case class C3(x: Int) extends Component
 
 class IncrementC1System extends System:
 
+  private def incrementC1(entity: Entity): Entity =
+    entity.get[C1] match
+      case Some(c1) => entity.add(C1(c1.x + 1))
+      case None => entity
+
   def update(world: World): Unit =
-    world
-      .entitiesWithAtLeastComponents(ComponentTag[C1])
-      .foreach(e => world.addComponent(e, C1(e.get[C1].get.x + 1)))
+    world.entitiesWithAtLeastComponents(ComponentTag[C1])
+      .foreach(e => world.addComponent(e, incrementC1(e).get[C1].get))
