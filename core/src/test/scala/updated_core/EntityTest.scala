811ae2dd482bbfe7dda3ce23778c6998b886f003
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2024-10-26 14:33:24 +0200

    updating EntityTest

--- core/src/test/scala/updated_core/EntityTest.scala
+++ core/src/test/scala/updated_core/EntityTest.scala
@@ -15,14 +15,13 @@ class EntityTest extends AnyWordSpec with Matchers:
         entity2.id shouldBe a[Int]
 
       "allow a variable number of components" in:
-        Entity()
-        Entity(C1(1))
-        Entity(C1(1), C2(2))
-        Entity(C1(1), C2(2), C3(3))
+        Entity().componentTags should have size 0
+        Entity(C1(1)).componentTags should have size 1
+        Entity(C1(1), C2(2)).componentTags should have size 2
+        Entity(C1(1), C2(2), C3(3)).componentTags should have size 3
 
       "not have multiple component of the same type" in:
-        val entity = Entity(C1(1), C1(2))
-        entity.componentTags.size shouldBe 1
+        Entity(C1(1), C1(2)).componentTags should have size 1
 
     "managing components" should:
       "allow adding a new component" in:
@@ -31,7 +30,7 @@ class EntityTest extends AnyWordSpec with Matchers:
 
       "allow retrieving an existing component" in:
         val entity = Entity(C1(1))
-        entity.get[C1] should matchPattern { case Some(C1(1)) => }
+        entity.get[C1] shouldBe Some(C1(1))
 
       "allow removing a component" in:
         val entity        = Entity(C1(1))
@@ -41,4 +40,4 @@ class EntityTest extends AnyWordSpec with Matchers:
       "updating a component" in:
         val entity        = Entity(C1(1))
         val updatedEntity = entity.add(C1(2))
-        updatedEntity.get[C1] should matchPattern { case Some(C1(2)) => }
+        updatedEntity.get[C1] shouldBe Some(C1(2))
