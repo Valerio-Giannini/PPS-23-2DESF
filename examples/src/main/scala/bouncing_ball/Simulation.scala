b45c954fa12ca5048a0fcb0e3c5bae86d5e707f8
rares vasiliu <rv.vasiliu@gmail.com>
2024-10-30 00:03:34 +0100

    simulation with the new DSL

--- examples/src/main/scala/bouncing_ball/Simulation.scala
+++ examples/src/main/scala/bouncing_ball/Simulation.scala
@@ -1,9 +1,10 @@
 package bouncing_ball
 
-import core.{ComponentTag, World}
+
+import dsl.DSL.*
 
 object Simulation:
-  private val world = World()
+  private val world = newWorld
 
   def initializeWorld(): Unit =
     val entity1 = world.createEntity(Position(0, 0), Speed(1, 1))
@@ -18,15 +19,6 @@ object Simulation:
     for tick <- 1 to 10 do
       println(s"Tick $tick")
       world.update()
-      for entity <- world.entitiesWithAtLeastComponents(ComponentTag[Position], ComponentTag[Speed]).toSeq.sortBy(_.id)
-      do
-        (entity.get[Position], entity.get[Speed]) match
-        case (Some(pos), Some(speed)) =>
-          val positionInfo = s"Position(${pos.x}, ${pos.y})"
-          val speedInfo    = s"Speed(${speed.vx}, ${speed.vy})"
-          println(s"Entity ${entity.id}: $positionInfo, $speedInfo")
-        case _ => println("Position Or Speed not found")
-      println("-------------------")
 
   @main def runSimulation(): Unit =
     start()
