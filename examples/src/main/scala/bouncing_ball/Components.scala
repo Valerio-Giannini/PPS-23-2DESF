3f4c63962d2c4c92efe92e7c6d96cdb7ed5c22a7
rares vasiliu <rv.vasiliu@gmail.com>
2024-10-30 00:02:37 +0100

    added vars for component tags

--- examples/src/main/scala/bouncing_ball/Components.scala
+++ examples/src/main/scala/bouncing_ball/Components.scala
@@ -1,6 +1,9 @@
 package bouncing_ball
 
-import core.Component
+import core.{Component, ComponentTag}
 
 case class Position(x: Double, y: Double) extends Component
 case class Speed(vx: Double, vy: Double)  extends Component
+
+val POSITION = ComponentTag[Position]
+val SPEED = ComponentTag[Speed]
