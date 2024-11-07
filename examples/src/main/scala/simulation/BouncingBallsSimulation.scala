d3f593ac9bc0b90aeab54edaad6f7d48bf5d2534
rares vasiliu <rv.vasiliu@gmail.com>
2024-11-07 15:37:46 +0100

    first version on the logic to run a simulation with the report and the statistics

---
+++ examples/src/main/scala/simulation/BouncingBallsSimulation.scala
@@ -0,0 +1,51 @@
+package simulation
+
+import dsl.coreDSL.CoreDSL.into
+import simulation.SimulationParameters.{ballRadius, borderSize, deceleration}
+
+object BounceSimulation extends Simulation:
+  override def condition: Boolean = Stats.calcAvgSpeed != 0
+
+  override def setParams: Unit =
+    deceleration = 0.05
+    borderSize = 290
+    ballRadius = 5
+
+  override def initWorld: Unit =
+    // maybe we should add a function where to put this block ?
+    into(world).include(MovementSystem())
+    into(world).include(BoundaryBounceSystem())
+    into(world).include(CollisionSystem())
+    into(world).include(PrintPositionAndSpeedOfEntitiesSystem())
+
+    val positions = List(
+      (-200, 100), (-150, -150), (0, 0), (100, 150), (150, -100),
+      (-100, -50), (200, 0), (-50, 200), (50, -200), (0, 100)
+    )
+
+    val velocities = List(
+      (10, 15), (-15, -10), (20, -20), (-10, 10), (15, 5),
+      (5, -15), (-20, 20), (10, 10), (15, -5), (-5, -20)
+    )
+    // end block
+
+    for ((pos, vel) <- positions.zip(velocities)) do
+      into(world).spawnNewEntityWith(Position(pos._1, pos._2), Speed(vel._1, vel._2))
+
+  override def showStats: Unit =
+    println(s"Velocità media ${Stats.calcAvgSpeed}")
+    println(s"Numero di entità in movimento ${Stats.numberOfMovingBalls}")
+    println(s"Numero di entità ferme ${Stats.numberOfStoppedBalls}")
+
+  override def updateReport: Unit =
+    Report.updateAvgSpeed(tick, Stats.calcAvgSpeed)
+    Report.updateMovingBalls(tick, Stats.numberOfMovingBalls)
+
+  override def showReport: Unit =
+    Report.showAvgSpeed()
+    Report.showMovingBalls()
+
+
+
+@main def run(): Unit =
+  BounceSimulation.init
---
+++ examples/src/main/scala/simulation/Components.scala
@@ -0,0 +1,9 @@
+package simulation
+
+import core.{Component, ComponentTag}
+
+case class Position(x: Double, y: Double) extends Component
+case class Speed(vx: Double, vy: Double)  extends Component
+
+val POSITION = ComponentTag[Position]
+val SPEED = ComponentTag[Speed]
