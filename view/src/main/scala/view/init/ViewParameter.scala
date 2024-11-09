d0125118c027cef8f0892deb99322484b5618a36
rares vasiliu <rv.vasiliu@gmail.com>
2024-11-09 09:42:51 +0100

    make label s a required parameter of ViewParameter

--- view/src/main/scala/view/init/ViewParameter.scala
+++ view/src/main/scala/view/init/ViewParameter.scala
@@ -1,8 +1,8 @@
 package view.init
 
-// Definizione di ViewParameter per gestire i parametri con id, value, minValue, maxValue e label opzionali
+  // Definizione di ViewParameter per gestire i parametri con id, value, minValue, maxValue e label opzionali
 case class ViewParameter(
-                          label: Option[String] = None,
+                          label: String,
                           value: AnyVal,
                           minValue: Option[AnyVal] = None,
                           maxValue: Option[AnyVal] = None
