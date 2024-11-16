439e111ea7b9a9ab6e537ef3563ccea481e37486
rares vasiliu <rv.vasiliu@gmail.com>
2024-11-16 17:56:16 +0100

    make labels of axis option

--- core/shared/src/main/scala/mvc/model/Report.scala
+++ core/shared/src/main/scala/mvc/model/Report.scala
@@ -2,7 +2,7 @@ package mvc.model
 
 case class Point(x: AnyVal, y: AnyVal)
 
-case class DataTrackerEntry(labelX: String, labelY: String, points: List[Point])
+case class DataTrackerEntry(labelX: Option[String], labelY: Option[String], points: List[Point])
 
 trait DataTracker:
   private var data: List[Point] = List.empty
@@ -17,17 +17,17 @@ case class ReportEntry(label: String, data: DataTrackerEntry)
 trait Report:
   def data: List[ReportEntry]
 
-  def addReportEntry(label: String, labelX: String, labelY: String, data: DataTracker): Unit
+  def addReportEntry(label: String, data: DataTracker, labelX: Option[String] = None, labelY: Option[String] = None): Unit
 
 object Report:
   def apply(): Report = new ReportImpl
 
   private class ReportImpl extends Report:
     private var report: List[(String, DataTracker)] = List.empty
-    private var axisLabels: (String, String) = _
+    private var axisLabels: (Option[String], Option[String]) = _
     override def data: List[ReportEntry] = report.map((label, dataTracker) => ReportEntry(label, DataTrackerEntry(axisLabels._1, axisLabels._2, dataTracker.get)))
 
-    override def addReportEntry(label: String, labelX: String, labelY: String, data: DataTracker): Unit =
+    override def addReportEntry(label: String, data: DataTracker, labelX: Option[String] = None, labelY: Option[String] = None): Unit =
       report = report :+ (label, data)
       axisLabels= (labelX, labelY)
 
