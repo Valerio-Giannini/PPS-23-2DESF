502deb98da7b60b85bae4aac931d9fb65d3d3218
rares vasiliu <rv.vasiliu@gmail.com>
2024-11-17 15:24:16 +0100

    minor change to report

--- core/shared/src/main/scala/mvc/model/Report.scala
+++ core/shared/src/main/scala/mvc/model/Report.scala
@@ -2,15 +2,45 @@ package mvc.model
 
 case class Point(x: AnyVal, y: AnyVal)
 
-case class DataTrackerEntry(labelX: Option[String], labelY: Option[String], points: List[Point])
-
 trait DataTracker:
   private var data: List[Point] = List.empty
 
   def add(x: AnyVal, y: AnyVal): Unit =
     data = data :+ Point(x, y)
 
-  def get: List[Point] = data
+  def points: List[Point] = data
+
+case class ReportEntry(label: String, labelX: Option[String] = None, labelY: Option[String] = None, var points: List[Point] = List.empty)
+
+trait Report:
+  def data: List[ReportEntry]
+  def addReportEntry(data: DataTracker, entry: ReportEntry): Unit
+
+object Report:
+  def apply(): Report = new ReportImpl
+
+  private class ReportImpl extends Report:
+    private var report: List[(DataTracker, ReportEntry)] = List.empty
+
+    override def addReportEntry(data: DataTracker, entry: ReportEntry): Unit =
+      report = report :+ (data, entry)
+
+
+    override def data: List[ReportEntry] =
+      report.map((data, entry) => 
+        entry.points = data.points
+        entry
+      )
+
+
+//    override def addReportEntry(label: String, data: DataTracker, labelX: Option[String] = None, labelY: Option[String] = None): Unit =
+//      report = report :+ (label, data)
+//      axisLabels= (labelX, labelY)
+
+
+
+/*
+case class DataTrackerEntry(labelX: Option[String] = None, labelY: Option[String] = None, points: List[Point])
 
 case class ReportEntry(label: String, data: DataTrackerEntry)
 
@@ -30,5 +60,6 @@ object Report:
     override def addReportEntry(label: String, data: DataTracker, labelX: Option[String] = None, labelY: Option[String] = None): Unit =
       report = report :+ (label, data)
       axisLabels= (labelX, labelY)
+ */
 
 
