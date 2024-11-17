068934872a8e4d3b6480594c6d7f81a48eabc526
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2024-11-17 17:22:06 +0100

    Adapt ArchetypeTest to new Components logic

--- core/jvm/src/test/scala/core/ArchetypeTest.scala
+++ core/jvm/src/test/scala/core/ArchetypeTest.scala
@@ -9,27 +9,34 @@ class ArchetypeTest extends AnyWordSpec with Matchers:
   "An Archetype" when:
     "created" should:
       "have a unique set of ComponentTag independent from order" in:
-        val archetype1 = Archetype(ComponentTag[C1], ComponentTag[C2])
-        val archetype2 = Archetype(ComponentTag[C2], ComponentTag[C1])
-        val archetype3 = Archetype(ComponentTag[C1], ComponentTag[C2], ComponentTag[C3])
+        val setC1C2 = summon[ComponentChainTag[C1 :: C2 :: CNil]].tags
+        val setC2C1 = summon[ComponentChainTag[C2 :: C1 :: CNil]].tags
+        val setC1C2C3 = summon[ComponentChainTag[C1 :: C2 :: C3 :: CNil]].tags
+
+        val archetype1 = Archetype(setC1C2)
+        val archetype2 = Archetype(setC2C1)
+        val archetype3 = Archetype(setC1C2C3)
 
         archetype1.componentTags shouldEqual archetype2.componentTags
         archetype1.componentTags should not equal archetype3.componentTags
     "managing entities" should:
       "allow to add only a matching entity" in:
-        val archetype = Archetype(ComponentTag[C1])
+        val setC1 = summon[ComponentChainTag[C1 :: CNil]].tags
+        val archetype = Archetype(setC1)
         val entity    = Entity(C1(1))
         archetype.add(entity)
         archetype.entities should contain(entity)
 
       "do nothing when trying to add a non matching entity" in:
-        val archetype = Archetype(ComponentTag[C1])
+        val setC1 = summon[ComponentChainTag[C1 :: CNil]].tags
+        val archetype = Archetype(setC1)
         val entity = Entity(C2(2))
         archetype.add(entity)
         archetype.entities shouldNot contain(entity)
 
       "do nothing when trying to add the same entity twice" in :
-        val archetype = Archetype(ComponentTag[C1])
+        val setC1 = summon[ComponentChainTag[C1 :: CNil]].tags
+        val archetype = Archetype(setC1)
         val entityA    = Entity(C1(1))
         archetype.add(entityA)
         val archetypeWithEntityA = archetype.entities
@@ -37,7 +44,8 @@ class ArchetypeTest extends AnyWordSpec with Matchers:
         archetype.entities shouldBe archetypeWithEntityA
 
       "allow to get an entity" in:
-        val archetype = Archetype(ComponentTag[C1])
+        val setC1 = summon[ComponentChainTag[C1 :: CNil]].tags
+        val archetype = Archetype(setC1)
         val entity    = Entity(C1(1))
 
         archetype.add(entity)
@@ -50,8 +58,9 @@ class ArchetypeTest extends AnyWordSpec with Matchers:
         val retrievedEntity2 = archetype.get(entity2)
         retrievedEntity2 shouldBe None
       "allow to remove an entity" in:
-        val archetype = Archetype(ComponentTag[C1])
-        val entity    = Entity(C1(1))
+        val setC1 = summon[ComponentChainTag[C1 :: CNil]].tags
+        val archetype = Archetype(setC1)
+        val entity    = Entity(C1(1) :: CNil)
 
         archetype.add(entity)
         archetype.remove(entity)
@@ -61,7 +70,8 @@ class ArchetypeTest extends AnyWordSpec with Matchers:
         archetype.remove(entity2)
         archetype.entities should not contain entity2
       "allow to clear all entities" in:
-        val archetype = Archetype(ComponentTag[C1])
+        val setC1 = summon[ComponentChainTag[C1 :: CNil]].tags
+        val archetype = Archetype(setC1)
         val entity1   = Entity(C1(1))
         val entity2   = Entity(C1(2))
 
